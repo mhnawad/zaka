f053c455f0bd750ab4391bdcfe87087e246f2a77
mhnawad <awad_mohanad@yahoo.com>
2026-02-08 22:25:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def internal_error(error):
     return jsonify({"error": "خطأ في الخادم"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="127.0.0.1", port=5000, use_reloader=False)
